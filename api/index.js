4bf648028c4fe297efa49629bd312f19b7bf5fab
Priya Chouhan <priyachouhan23@navgurukul.org>
2025-05-29 07:50:49 +0530

    Added all code in same time again.

---
+++ api/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const path = require("path");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err.message);
+  });
+
+
+  const app = require('./server'); // this refers to your Express app
+const serverless = require('serverless-http');
+
+module.exports = serverless(app);
+
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Route imports
+const aiRoutes = require("./routes/explanations");
+
+// Use routes
+app.use("/api", aiRoutes);
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
+
+app.listen(port, () => {
+  console.log(`✅ Server running at http://localhost:${port}`);
+});
